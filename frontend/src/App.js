d553c156db68fc46311512a19c4b6d938d774a17
shalini-araja <n210499@rguktn.ac.in>
2025-10-13 16:12:43 +0530

    index changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -15,39 +15,48 @@ import OutsideCharity from './components/departments/OutsideCharity';
 import PublicRelations from './components/departments/PublicRelations';
 import AdminDashboard from './adminPortal/AdminDashboard';
 import AdminLogin from './adminPortal/adminLOgin';
-import ProtectedRoute from './components/ProtectetdRoute';
-function App() {
-  return (
-    <Router>
-      {/* Navbar will appear on all pages */}
-      <Navbar />
+import ProtectedRoute from './components/ProtectedRoute';
 
-      {/* Define routes for pages */}
+function AppWrapper() {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname === '/admin-login';
+
+  return (
+    <>
+      {!hideHeaderFooter && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
-         <Route path="/certificate" element={<Certificate />} />
-         <Route path="/gallery" element={<Gallery/>}/>
-         <Route path="/ourTeam" element={<Team/>}/>
-         <Route path="/healthCare" element={<HealthCare/>}/>
-         <Route path="/innovativeAndInformative" element={<InnovativeAndInformative/>}/>
-          <Route path="/internalCare" element={<InternalCare/>}/>
-          <Route path="/outsideCharity" element={<OutsideCharity/>}/>
-          <Route path="/publicRelations" element={<PublicRelations/>}/>
-         <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/certificate" element={<Certificate />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/ourTeam" element={<Team />} />
+        <Route path="/healthCare" element={<HealthCare />} />
+        <Route path="/innovativeAndInformative" element={<InnovativeAndInformative />} />
+        <Route path="/internalCare" element={<InternalCare />} />
+        <Route path="/outsideCharity" element={<OutsideCharity />} />
+        <Route path="/publicRelations" element={<PublicRelations />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
         <Route
           path="/admin-dashboard"
           element={
             <ProtectedRoute>
               <AdminDashboard />
-            </ProtectedRoute>}
-            />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-      
 
-      {/* Footer will appear on all pages */}
-      <Footer />
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppWrapper />
     </Router>
   );
 }
